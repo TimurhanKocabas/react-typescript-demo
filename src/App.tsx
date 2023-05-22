180b5c7a2a21f8ed6efc602f909b6c88e6210890
timurhan kocabas <timurhankocabas@outlook.com>
2023-05-22 16:03:01 +0200

    upto lesson 14

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button } from "./components/Button";
+import { Container } from "./components/Container";
+import { Greet } from "./components/Greet";
+import { Heading } from "./components/Heading";
+import { Input } from "./components/Input";
+import { Oscar } from "./components/Oscar";
+import { Status } from "./components/Status";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Status status={"error"}></Status>
+      <Heading> Placeholder text</Heading>
+      <Oscar>
+        <Heading> Oscar goes to Leonardo Dicaprio.</Heading>
+      </Oscar>
+      <Greet name="Timmy" messageCount={10} isLoggedIn={true}></Greet>
+      <Button
+        handleClick={( event, id) => {
+          console.log("Button Clicked.", event,id);
+        }}
+      ></Button>
+      <Input value='' handleChange = {(event) => console.log(event)}></Input>
+      <Container styles={{ border: "1px solid black", padding: "1rem" }}></Container>
     </div>
   );
 }
