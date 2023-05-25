7a39ea54941e71226e8718116561de93e165d434
timurhan kocabas <timurhankocabas@outlook.com>
2023-05-25 11:23:24 +0200

    getting rid of null check

--- src/components/context/User.tsx
+++ src/components/context/User.tsx
@@ -3,24 +3,20 @@ import { useContext } from "react";
 export const User = () => {
   const userContext = useContext(UserContext);
   const handleLogin = () => {
-    if(userContext){
-      userContext.setUser({
-        name:'timmy',
-        email:'timmy@timmy.com'
-      })
-    }
+    userContext.setUser({
+      name: "timmy",
+      email: "timmy@timmy.com",
+    });
   };
   const handleLogout = () => {
-    if(userContext){
-      userContext.setUser(null);
-    }
+    userContext.setUser(null);
   };
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
       <button onClick={handleLogout}>Logout</button>
-      <div>User name is {userContext?.user?.name}</div>
-      <div>User email is {userContext?.user?.email}</div>
+      <div>User name is {userContext.user?.name}</div>
+      <div>User email is {userContext.user?.email}</div>
     </div>
   );
 };
