2f5c30b7ca1a3ecd63eb275027d915e325161870
timurhan kocabas <timurhankocabas@outlook.com>
2023-05-25 11:21:30 +0200

    Context with a future value

---
+++ src/components/context/User.tsx
@@ -0,0 +1,26 @@
+import { UserContext } from "./UserContext";
+import { useContext } from "react";
+export const User = () => {
+  const userContext = useContext(UserContext);
+  const handleLogin = () => {
+    if(userContext){
+      userContext.setUser({
+        name:'timmy',
+        email:'timmy@timmy.com'
+      })
+    }
+  };
+  const handleLogout = () => {
+    if(userContext){
+      userContext.setUser(null);
+    }
+  };
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogout}>Logout</button>
+      <div>User name is {userContext?.user?.name}</div>
+      <div>User email is {userContext?.user?.email}</div>
+    </div>
+  );
+};
