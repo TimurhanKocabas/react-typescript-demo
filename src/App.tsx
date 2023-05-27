954179372b5f83e1dd992e969f3c9af48a683e9f
timurhan kocabas <timurhankocabas@outlook.com>
2023-05-27 11:32:57 +0200

    wrapping Html elements , omit, design system hints

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import { Toast } from "./components/templateliterals/Toast";
+import { CustomButton } from "./components/html/Button";
 
 function App() {
   return (
     <div className="App">
-     <Toast position="center-bottom" ></Toast>
+      <CustomButton variant="primary" onClick={()=>{console.log("clicked")}}>Primary Button</CustomButton>
     </div>
   );
 }
